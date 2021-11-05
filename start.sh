9ccff82fd8cd437003d726f460e39d8417c87d18
TheFSilverSP <80601861+TheFSilverSP@users.noreply.github.com>
2021-11-05 15:55:59 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # check if port variable is set or go with default
 if [ -z ${PORT+x} ]; then echo "PORT variable not defined, leaving N8N to default port."; else export N8N_PORT=$PORT; echo "N8N will start on '$PORT'"; fi
 
-export N8N_DIAGNOSTICS_ENABLED=false
+export N8N_DIAGNOSTICS_ENABLED=true
 
 # regex function
 parse_url() {
