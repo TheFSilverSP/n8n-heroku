466f67fb7cafb0747fae5d60d755a6e5dae2ba6c
TheFSilverSP <80601861+TheFSilverSP@users.noreply.github.com>
2022-06-14 16:00:40 +0200

    Update create-torrc.sh

--- create-torrc.sh
+++ create-torrc.sh
@@ -1,3 +1,3 @@
 echo "HiddenServiceDir ./hidden_service" >> ./hidden_service/torrc
-echo "HiddenServicePort 80 127.0.0.1:9050" >> ./hidden_service/torrc
+echo "HiddenServicePort 80 127.0.0.1:$PORT" >> ./hidden_service/torrc
 tor -f ./hidden_service
